fd3e40539573db980127480e99cbd4a5225f43a3
Alex Haines <alex@snowshoestamp.com>
2019-07-11 18:17:54 -0700

    Formatting code

--- jquery.snowshoe.js
+++ jquery.snowshoe.js
@@ -5,10 +5,10 @@ Version 0.3.2
 See GitHub project page for Documentation and License
 */
 
-(function($) {
+(function ($) {
   $.snowshoe = {
     stampScreen: {
-      init: function(configs, client){
+      init: function (configs, client) {
         var stampScreenElmId = configs.stampScreenElmId || "snowshoe-stamp-screen";
         var progressAnimationOn = configs.progressBarOn || false;
         var postViaAjax = configs.postViaAjax || false;
@@ -22,17 +22,18 @@ See GitHub project page for Documentation and License
         var points = [];
         var stampScreenElm = document.getElementById(stampScreenElmId);
         var stampTouching = false;
-		// override complete function to reset stampTouching if passed from configuration
-		var confComplete = complete;
-		complete = function() {
-		  confComplete.apply(this, arguments);
-		  stampTouching=false;
-		  return confComplete;
-		}
         var pointsMin = configs.insufficientPointsMin || 3;
         var holdMsg;
 
-        stampScreenElm.addEventListener('touchstart', function(event) {
+        // override complete function to reset stampTouching if passed from configuration
+        var confComplete = complete;
+        complete = function () {
+          confComplete.apply(this, arguments);
+          stampTouching = false;
+          return confComplete;
+        }
+
+        stampScreenElm.addEventListener('touchstart', function (event) {
           $("#snowshoe-messages").empty();
           clearTimeout(holdMsg);
           if (event.touches.length >= pointsMin && progressAnimationOn) {
@@ -50,12 +51,12 @@ See GitHub project page for Documentation and License
             send(data, postViaAjax);
           }
 
-          if (event.touches.length < 5 && event.touches.length >= pointsMin ) {
+          if (event.touches.length < 5 && event.touches.length >= pointsMin) {
             $('#snowshoe-progress-bar').removeClass("snowshoe-progress-bar");
             // Teach users to stamp and hold for 2 seconds before displaying user-defined message
-            if (helpMessage) { 
+            if (helpMessage) {
               $("#snowshoe-messages").append(holdMessage);
-              holdMsg = setTimeout(function(){ displayCustomMessage(helpMessage) }, 2000);
+              holdMsg = setTimeout(function () { displayCustomMessage(helpMessage) }, 2000);
             }
           }
         });
@@ -64,12 +65,12 @@ See GitHub project page for Documentation and License
           $("#snowshoe-messages").children().replaceWith(helpMsg);
         };
 
-        function send(points, postViaAjax){
+        function send(points, postViaAjax) {
           // return if already one process is working out
-          if(stampTouching) return false;
-          stampTouching=true;
-          if (postViaAjax){
-            client.postAjax(points, postUrl, success, error,complete);
+          if (stampTouching) return false;
+          stampTouching = true;
+          if (postViaAjax) {
+            client.postAjax(points, postUrl, success, error, complete);
           } else {
             client.post(points, postUrl);
           }
@@ -81,25 +82,24 @@ See GitHub project page for Documentation and License
       //
       // Coupled to $.snowshoe.Base64
       //
-      postAjax: function(data, endpoint, cbk, cbkError,cbkComplete) {
+      postAjax: function (data, endpoint, cbk, cbkError, cbkComplete) {
         $.ajax({
           'url': endpoint,
           'data': "data=" + $.snowshoe.Base64.encode(JSON.stringify(data)),
           'type': "POST",
-          'error': function(response) {
+          'error': function (response) {
             cbkError(response.responseJSON);
           },
-          'success': function(response) {
+          'success': function (response) {
             cbk(response);
           },
-          'complete': function(response){
-			cbkComplete(response);
-			
-		  }
+          'complete': function (response) {
+            cbkComplete(response);
+          }
         })
       },
 
-      post: function(data, endpoint) {
+      post: function (data, endpoint) {
         var form = $('<form method="POST"></form>');
         form.attr('action', endpoint);
         var input = $('<input name="data" type="hidden"></input>');
@@ -115,7 +115,7 @@ See GitHub project page for Documentation and License
       // Base64 adapted from & courtesy of http://scotch.io
       //
       _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-      encode : function (input) {
+      encode: function (input) {
         var output = "";
         var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
         var i = 0;
@@ -134,21 +134,21 @@ See GitHub project page for Documentation and License
             enc4 = 64;
           }
           output = output +
-          this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-          this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+            this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+            this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
         }
         return output;
       },
 
-      _utf8_encode : function (string) {
-        string = string.replace(/\r\n/g,"\n");
+      _utf8_encode: function (string) {
+        string = string.replace(/\r\n/g, "\n");
         var utftext = "";
         for (var n = 0; n < string.length; n++) {
           var c = string.charCodeAt(n);
           if (c < 128) {
             utftext += String.fromCharCode(c);
           }
-          else if((c > 127) && (c < 2048)) {
+          else if ((c > 127) && (c < 2048)) {
             utftext += String.fromCharCode((c >> 6) | 192);
             utftext += String.fromCharCode((c & 63) | 128);
           }
